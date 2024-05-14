c0403b70c36f3c344c20210b9b37fb8c101ae1b0
Jhonata Figueredo <jhonata@motoca.com.br>
2024-05-14 16:02:01 -0300

    feat: add a new testcase and test funcitions with node assertions

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
+import assert from "node:assert";
+
 const sumExtraHours = (salary, extraHours) => salary + extraHours;
 
 const calculateDiscount = (salary, discount) => salary - discount;
 
-const testCase = (title, expected, actual) => {
-  if (expected === actual) {
+const expect = (actual) => {
+  const assertions = {
+    toEqual: (expected) => {
+      assert.strictEqual(actual, expected);
+    },
+  };
+
+  return assertions;
+};
+
+const test = (title, callback) => {
+  try {
+    callback();
+
     console.log(`\n ${title} passed`);
-  } else {
+  } catch (error) {
+    console.error(error);
     console.error(`\n ${title} failed`);
   }
 };
 
-testCase('sum extra hours', 2500, sumExtraHours(2000, 500));
+test("sum extra hours", () => {
+  const expected = 2500;
+  const actual = sumExtraHours(2000, 500);
 
-testCase('calculate discount', 2200, calculateDiscount(2500, 300));
+  expect(actual).toEqual(expected);
+});
+
+test("calculate discount", () => {
+  const expected = 2200;
+  const actual = calculateDiscount(2500, 300);
+
+  expect(actual).toEqual(expected);
+});
