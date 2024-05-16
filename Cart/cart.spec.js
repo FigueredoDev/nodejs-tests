c12a133a238927be88ca981bcd381de5f3a577e8
JhonataFigueredo <jhowfigueredo2@gmail.com>
2024-05-15 21:19:31 -0300

    test: add Cart and Item unit tests

--- Cart/cart.spec.js
+++ Cart/cart.spec.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Cart from "./cart"
 import Item from "./item"
 
@@ -29,3 +30,42 @@ describe('Class Cart', () => {
         expect(cart.total).toBeNull()
     })
 })
+=======
+import Cart from "./cart";
+import Item from "./item";
+
+describe("Class Cart", () => {
+  it("should initialize empty", () => {
+    const cart = new Cart();
+
+    expect(cart.subtotal).toBeNull();
+  });
+
+  it("should have items", () => {
+    const apple = new Item("Apple", 2, 2);
+    const banana = new Item("Banana", 3, 3);
+
+    const cart = new Cart();
+
+    cart.add(apple);
+    cart.add(banana);
+
+    expect(cart.items.length).toBe(2);
+    expect(cart.items).toContain(apple);
+    expect(cart.items).toContain(banana);
+  });
+
+  it('should contain a total property on initialization', () => {
+        const cart = new Cart()
+
+        expect(cart).toHaveProperty('total')
+        expect(cart.total).toBeNull()
+    })
+
+  it("should throw an error when cart is empty", () => {
+    const cart = new Cart();
+
+    expect(() => cart.finishPurchase()).toThrowError("Cart is empty");
+  });
+});
+>>>>>>> 78dfc0f (test: add Cart and Item unit tests)
