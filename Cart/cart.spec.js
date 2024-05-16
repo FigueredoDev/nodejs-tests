b96b771fea32891d6ee4d4c62bf83436fc698a0c
Jhonata Figueredo <jhonata@motoca.com.br>
2024-05-16 09:26:11 -0300

    test: add new Cart unit tests

--- Cart/cart.spec.js
+++ Cart/cart.spec.js
@@ -1,4 +1,3 @@
-
 import Cart from "./cart";
 import Item from "./item";
 
@@ -23,16 +22,54 @@ describe("Class Cart", () => {
     expect(cart.items).toContain(banana);
   });
 
-  it('should contain a total property on initialization', () => {
-        const cart = new Cart()
-
-        expect(cart).toHaveProperty('total')
-        expect(cart.total).toBeNull()
-    })
-
-  it("should throw an error when cart is empty", () => {
+  it("should contain a total property on initialization", () => {
     const cart = new Cart();
 
-    expect(() => cart.finishPurchase()).toThrowError("Cart is empty");
+    expect(cart).toHaveProperty("total");
+    expect(cart.total).toBeNull();
   });
-})
+
+  describe('Method "finishPurchase"', () => {
+    it("should finish purchase", () => {
+      const cart = new Cart();
+      const apple = new Item("apple", 10, 5);
+
+      cart.add(apple);
+      cart.addShippingValue(10);
+
+      expect(cart.finishPurchase()).toStrictEqual({
+        subtotal: 50,
+        shippingValue: 10,
+        total: 60,
+      });
+    });
+
+    it("should throw an error when cart is empty", () => {
+      const cart = new Cart();
+
+      expect(() => cart.finishPurchase()).toThrowError("Cart is empty");
+    });
+  });
+
+  describe("Method 'calculateTotal'", () => {
+    it("should calculate total items value", () => {
+      const cart = new Cart();
+      const apple = new Item("apple", 10, 5);
+
+      cart.add(apple);
+
+      expect(cart.calculateTotal()).toEqual(50);
+    });
+  });
+
+  describe("Method 'addShippingValue'", () => {
+    it("should add shipping value", () => {
+      const cart = new Cart();
+      const expected = 100;
+
+      cart.addShippingValue(100);
+
+      expect(cart.shippingValue).toEqual(expected);
+    });
+  });
+});
