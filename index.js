a00f511c76f38413e9255f0f9b9591bcfb057e5a
Jhonata Figueredo <jhonata@motoca.com.br>
2024-05-22 11:18:00 -0300

    chore: remove old files

--- index.js
+++
@@ -1,5 +0,0 @@
-const sumExtraHours = (salary, extraHours) => salary + extraHours;
-
-const calculateDiscount = (salary, discount) => salary - discount;
-
-export { sumExtraHours, calculateDiscount };
